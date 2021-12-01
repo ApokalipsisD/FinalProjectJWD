294ad9de64119241ccbb3d67e3e312335723afbb
Sergey Baichik <sergey.baichyk@yandex.ru>
2021-12-02 01:42:40 +0300

    upd

--- src/main/java/com/epam/jwd/controller/command/ApplicationCommand.java
+++ src/main/java/com/epam/jwd/controller/command/ApplicationCommand.java
@@ -17,8 +17,8 @@ public enum ApplicationCommand {
     SHOW_LOGIN(ShowLoginPageCommand.getInstance()),
     SHOW_MAIN(ShowMainPageCommand.getInstance()),
     SHOW_SIGN_UP(ShowSignUpPageCommand.getInstance()),
-    SIGN_UP_COMMAND(SignUpCommand.getInstance()),
-    LOGIN();
+    SIGN_UP_COMMAND(SignUpCommand.getInstance());
+
 
     private final Command command;
     private final List<Role> allowRoles;
